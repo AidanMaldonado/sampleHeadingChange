7ab7210bbfd2697747c97d04480d0d1dc4bd3445
AidanMaldonado <aidanm1198@gmail.com>
2018-04-19 18:47:03 -0400

    Added JS, HTML & CSS files

---
+++ script.js
@@ -0,0 +1,12 @@
+const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
+const myTextInput = document.getElementById('myTextInput');
+const myButton2 = document.getElementById('myButton2');
+
+myButton.addEventListener('click', () => {
+  myHeading.style.color = myTextInput.value;
+});
+
+myButton2.addEventListener('click', () => {
+  myHeading.style.color = "black";
+});
